b02e206e3a2f1c3f528cf73236e171a9a60d7c0b
shen-c <congshen@cpp.edu>
2022-10-23 16:08:45 -0700

    initial outline

---
+++ restaurantBill.c
@@ -0,0 +1,14 @@
+
+int main(void){
+
+    //create values for menu items
+
+    //create a random array that correlates with each menu item
+
+    //randomly generate a meal by matching menu items to respective random num in array
+
+    //get inputs for tax and tip
+
+    //calculate totals and display
+
+}
